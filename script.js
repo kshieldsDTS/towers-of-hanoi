42e90b42c3012821f1b8768330fee75dccb1d122
kshieldsDTS <escanor05@gmail.com>
2021-12-07 15:14:55 -0500

    complete HTML and CSS visualization

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log('hello');
+// CONSTANTS
+const towers = document.querySelector('.towers');
+const banner = document.querySelector('h2')
+// VARIABLES
+
+// LISTENERS
+document.addEventListener('click', handleClick);
+// FUNCTIONS
+function handleClick(event) {
+    if (event.target.classList.contains('towers'))
+        console.log(event.target);
+}
